2e8cd511beb740e628e0cc65333bf0d9fc68d3e9
Volodymyr Baginskyi <vova27n@gmail.com>
2023-07-27 22:24:45 +0300

    add some Java files

---
+++ dz-10/src/main/java/org/example/exceptions/WrongAccountException.java
@@ -0,0 +1,5 @@
+package org.example.exceptions;
+
+public class WrongAccountException extends Exception
+{
+}
---
+++ dz-10/src/main/java/org/example/exceptions/WrongOperationException.java
@@ -0,0 +1,5 @@
+package org.example.exceptions;
+
+public class WrongOperationException extends Exception
+{
+}
